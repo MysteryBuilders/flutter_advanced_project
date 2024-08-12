049db91cc6d6f5b3c1f4a2f9532ad780cbb850be
Mohammed Elshwehy <eng.melshwehy@gmail.com>
2024-08-12 12:49:06 +0300

    boarding

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:apps/core/routing/routes.dart';
+import 'package:apps/features/login/ui/login_screen.dart';
+import 'package:apps/features/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter{
+  Route generateRoute(RouteSettings settings){
+    switch(settings.name){
+      case Routes.onBoardingScreen:
+       return MaterialPageRoute(builder: (_)=>const OnboardingScreen(),);
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_)=>const LoginScreen(),);
+      default :
+        return MaterialPageRoute(builder: (_)=>
+        Scaffold(
+          body: Center(
+            child: Text('No Route Defined for ${settings.name!}'),
+          ),
+        ));
+    }
+
+  }
+}
